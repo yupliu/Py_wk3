cf9e612b98fe4b83d7e44ba0643c446982e57421
Alan Yu <yupliu@users.noreply.github.com>
2017-07-10 10:46:58 -0400

    Word frequency

--- Py_wk3/Py_wk3.py
+++ Py_wk3/Py_wk3.py
@@ -46,19 +46,14 @@ for word in selected_words:
     #retrieve the index of the word in ftr list
     word_index = ftrname.index(word)
     selected_index.append(word_index)    
-#associate word with index
+#associate word with index, built two dictionary word to index and index to word
 ftr_selected = dict(zip(selected_words,selected_index))
+ftr_selected_rev = dict(zip(selected_index,selected_words))
 #extract ftr matrix ???
 ftr_train = ftrmat[:,selected_index]
 #split the data into trian and test 0.8
 from sklearn.cross_validation import train_test_split
 xtrain,xtest,ytrain,ytest = train_test_split(ftr_train,data_label,test_size=0.2)
-#Normalize the attribute value to mean=0 and sd =1
-#from sklearn.preprocessing import StandardScaler
-#scalar = StandardScaler()
-#scalar.fit(xtrain)
-#xtrain = scalar.transform(xtrain)
-#xtest = scalar.transform(xtest)
 #build the classfier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import precision_recall_curve, f1_score
@@ -75,11 +70,27 @@ fig.canvas.set_window_title("Amazon product classifier")
 plt.title('Precision-Recall Curves')
 plt.xlabel('Precision')
 plt.ylabel('Recall')
+label = 'Logistic regression F1 score = {:.3f}'.format(score)
+#label object has to be defined first otherwise plt.legend will cause a warning
+plt.plot(recall,precision,label=label)
 plt.legend(loc='best')
 #plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,ncol=2, mode="expand", borderaxespad=0.)
-plt.plot(recall,precision,label=label)
 plt.show()
 plt.close()
+#calculate the frequency of each word in select words
+ftr_train_freq = ftr_train.toarray().sum(axis=0).tolist()
+#get the least and most frequent word
+minword = ftr_selected_rev[selected_index[ftr_train_freq.index(min(ftr_train_freq))]]
+maxword = ftr_selected_rev[selected_index[ftr_train_freq.index(max(ftr_train_freq))]]
+print("Least frequent word is ",minword)
+print("Most frequent word is ",maxword)
+#get the weight from the model
+coefficent = classifier.coef_[0].tolist()
+minword = ftr_selected_rev[selected_index[coefficent.index(min(coefficent))]]
+maxword = ftr_selected_rev[selected_index[coefficent.index(max(coefficent))]]
+print("Most negative word is ",minword)
+print("Most positive word is ",maxword)
+
 
 
 
